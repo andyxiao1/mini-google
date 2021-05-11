d183332032c641785306d0dfdb6d1ea5e58734f7
Andy Xiao <andyxiao@seas.upenn.edu>
2021-05-11 00:30:45 -0400

    Fix cors issue

--- query_server/src/main/java/edu/upenn/cis/cis455/WebServer.java
+++ query_server/src/main/java/edu/upenn/cis/cis455/WebServer.java
@@ -22,22 +22,22 @@ import spark.Spark;
 
 
 public class WebServer {
-	
+
 	private static final Logger logger = LogManager.getLogger(WebServer.class);
-	
+
     public static void main(String[] args) {
         org.apache.logging.log4j.core.config.Configurator.setLevel("edu.upenn.cis.cis455", Level.INFO);
-        
+
         int port = Integer.parseInt(args[0]);
-        
+
         Spark.port(port);
-        
+
         String tableName = args[1];
-        
+
         int maxLimitReturn = Integer.parseInt(args[2]);
-        
+
     	String pageRankTableName = args[3];
-        
+
         ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
         try {
             credentialsProvider.getCredentials();
@@ -46,33 +46,37 @@ public class WebServer {
                     + "Please make sure that your credentials file is at the correct "
                     + "location (/home/.aws/credentials), and is in valid format.", e);
         }
-        
+
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
         		.withRegion(Regions.US_EAST_1).withCredentials(credentialsProvider).build();
-        
+
         DynamoDB dynamoDB = new DynamoDB(client);
-        
+
         Table table = dynamoDB.getTable(tableName);
-        
+
         Table pageRankTable = dynamoDB.getTable(pageRankTableName);
-        
+
         // DOCUMENT TABLE IS HARD CODED
         Table docTable = dynamoDB.getTable("documents-final");
-        
+
         System.out.println("Starting server on port: " + port);
-        
+
 //        logger.debug("Starting logger");
-        
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Methods", "GET");
+        });
+
         get("/", (request, response) -> "hello world");
-        
+
         get("/hello", (request, response) -> "world");
-        
+
         get("/search", new SearchHandler(table, maxLimitReturn, pageRankTable, docTable, dynamoDB));
-        
+
         get("/shutdown", (request, response) -> {
         	stop();
         	return "";
         });
-        
+
     }
 }
